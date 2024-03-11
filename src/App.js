bf731e5ea705b28c59a254aa51351e47830ef975
yonghoon1013 <jyh10105@gmail.com>
2024-03-12 02:04:29 +0900

    ff

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import My from './pages/My';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path='/' element={<Home/>}></Route>
         <Route path='list' element={<List/>}></Route>
