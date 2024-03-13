b826da958c57459caaec1784e03f551a03d48b82
yonghoon1013 <jyh10105@gmail.com>
2024-03-14 01:13:42 +0900

    gg

--- src/pages/List.js
+++ src/pages/List.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 function List() {
     return (
         <div>List
-            <Link to={'/'}>홈으로</Link>
+            <Link to={'/'}>홈으로가자</Link>
         </div>
     )
 }
